c7ff24812259748f12e73d40fd1bcc0528f4f2a2
mcjara <moises@trustportal.org>
2024-03-14 13:06:44 +0000

    Fixing small error

--- main.tf
+++ main.tf
@@ -106,12 +106,12 @@ resource "aws_security_group" "vm" {
   }
 
   dynamic "ingress" {
-    for_each = var.application_ports
+    for_each = var.vm_ports
     content {
       from_port       = ingress.value
       to_port         = ingress.value
       protocol        = "tcp"
-      security_groups = [aws_security_group.alb.id]
+      security_groups = [aws_security_group.lb.id]
     }
   }
 
